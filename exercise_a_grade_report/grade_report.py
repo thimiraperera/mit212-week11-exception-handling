f81f68869c3c9b86e2d421000100e4b4ffbc9ec3
Thimira Perera <thimiraperera10@gmail.com>
2026-07-05 18:16:18 +0530

    Report skipped marks

--- exercise_a_grade_report/grade_report.py
+++ exercise_a_grade_report/grade_report.py
@@ -2,13 +2,24 @@ students = {
     "S001": 78, "S002": 55, "S003": None,
     "S004": 91, "S005": "63", "S006": 82
 }
+
 def class_report(data):
     marks = []
+    skipped_count = 0
 
-    for mark in data.values():
-        if mark is not None:
+    for student_id, mark in data.items():
+        if mark is None:
+            skipped_count += 1
+            print(student_id, "skipped: missing mark")
+            continue
+
+        try:
             mark = int(mark)
             marks.append(mark)
+
+        except ValueError:
+            skipped_count += 1
+            print(student_id, "skipped: mark is not a valid number")
 
     # marks = list(data.values())
 
@@ -20,6 +31,7 @@ def class_report(data):
     print("Average:", average)
     print("Highest:", highest)
     print("Lowest:", lowest)
+    print("Skipped records:", skipped_count)
 
     # print(len(marks))
 
