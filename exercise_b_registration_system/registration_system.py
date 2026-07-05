ae5c1f6f706e219b3be46fbb9ae7970776697bf9
Thimira Perera <thimiraperera10@gmail.com>
2026-07-05 18:34:37 +0530

    Add DuplicateStudentError check

--- exercise_b_registration_system/registration_system.py
+++ exercise_b_registration_system/registration_system.py
@@ -1,6 +1,9 @@
 class InvalidMarkError(Exception):
     pass
 
+class DuplicateStudentError(Exception):
+    pass
+
 students = {
     "S001": 78,
     "S002": 55,
@@ -10,13 +13,17 @@ students = {
     "S006": 82
 }
 
-def set_mark(student_id, mark):
+def set_mark(student_id, mark, overwrite=False):
     mark = int(mark)
 
     if mark < 0 or mark > 100:
         raise InvalidMarkError("Mark must be between 0 and 100")
 
+    if student_id in students and overwrite == False:
+        raise DuplicateStudentError("Student ID already exists")
+
     students[student_id] = mark
 
-set_mark("S007", 75)
+set_mark("S001", 80, overwrite=True)
+
 print(students)
