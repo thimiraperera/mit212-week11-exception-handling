d735e19e510eab271d9d5a0d269ad00e762990eb
Thimira Perera <thimiraperera10@gmail.com>
2026-07-05 18:11:50 +0530

    Convert string marks and skip none marks

--- exercise_a_grade_report/grade_report.py
+++ exercise_a_grade_report/grade_report.py
@@ -3,13 +3,24 @@ students = {
     "S004": 91, "S005": "63", "S006": 82
 }
 def class_report(data):
-    marks = list(data.values())
+    marks = []
+
+    for mark in data.values():
+        if mark is not None:
+            mark = int(mark)
+            marks.append(mark)
+
+    # marks = list(data.values())
+
     total = sum(marks)
     average = total / len(marks)
     highest = max(marks)
     lowest = min(marks)
+
     print("Average:", average)
     print("Highest:", highest)
     print("Lowest:", lowest)
+
+    # print(len(marks))
 
 class_report(students)
